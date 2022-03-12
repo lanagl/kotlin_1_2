b1f5e2496bd6318c0e2bf3a78f552b426d82039d
lanagl <lanaglaz@gmail.com>
2022-03-12 19:01:00 +0300

    initial commit task People

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,11 @@
-fun main(){
-    val amount = 30020_00
-    val percentCommission = amount * 75 / 100_00
+fun main() {
+    val likes = 112
 
-    val commission = if (percentCommission < 35_00) 35_00 else percentCommission
+    val peopleSome = "человеку"
+    val peopleMany = "людям"
 
-    println("${commission / 100} рублей ${commission % 100} копеек")
+    val people = if (likes % 10 == 1 && (likes < 11 || likes > 20)) peopleSome else peopleMany
+
+    println("Понравилось $likes $people")
 
 }
